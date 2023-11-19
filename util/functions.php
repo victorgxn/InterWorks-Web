9ed9c3703119249c6ffaff0fe4e023b1530ba3df
Victor Gonzalez <134617120+victorgxn@users.noreply.github.com>
2023-11-19 12:21:55 +0100

    Remove util from tracking

--- util/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-function depurar($cadena)
-{
-  $string = preg_replace(['/\s+/', '/^\s|\s$/'], [' ', ''], $cadena);
-  $string = trim($string);
-  $string = stripslashes($string);
-  $string = htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
-  return $string;
-}
